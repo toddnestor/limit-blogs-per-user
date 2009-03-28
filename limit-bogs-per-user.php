e4efd7fc35f50913962e77c8c944d86b328e3c63
write2brajesh <write2brajesh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-28 08:32:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/limit-blogs-per-user/trunk@105844 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ limit-bogs-per-user.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin name:Limit Blogs per User
+Plugin Author:Brajesh K. Singh
+Plugin URI:http://www.thinkinginwordpress.com/2009/03/limit-number-of-blogs-per-user-for-wordpress-mu-and-buddypress-websiteblog-network/
+Author URI:http://ThinkingInWordpress.com
+Version:1.0
+*/
+
+add_filter("wpmu_active_signup","tiw_check_current_users_blog"); //send fake/true enable or disabled request
+add_action("wpmu_options","tiw_display_options_form"); //show the form to allow how many number of blogs per user
+add_action("update_wpmu_options","tiw_save_num_allowed_blogs");//action to save number of allowed blogs per user
+
+/****Check ,whether blog registration is allowed,and how many blogs per logged in user is allowed */
+
+function tiw_check_current_users_blog($active_signup)
+{
+	if( !is_user_logged_in() )
+	return $active_signup;//if the user is not logged in,do not change the site policies
+
+	//otherwise...
+	global $current_user;
+	$blogs=get_blogs_of_user($current_user->ID);//get all blogs of user
+	
+	$number_of_blogs_per_user=tiw_num_allowed_blogs();//find 
+	
+	//if number of allowed blog is greater than 0 and current user owns less number of blogs */
+	if($number_of_blogs_per_user>0&&count($blogs)<$number_of_blogs_per_user)
+			return $active_signup;
+	else
+	return "none";
+}
+
+/****How many blogs are allowed per user *************/
+
+function tiw_num_allowed_blogs()
+{
+$num_allowed_blog=get_site_option("tiw_allowed_blogs_per_user");//find how many blogs are allowed
+if(!isset($num_allowed_blog))
+$num_allowed_blog=0;
+
+return $num_allowed_blog;//return the number of allowed blogs
+}
+
+/*****Show the Number of Blogs to restrict per user at the bottom of Site options ****/
+function tiw_display_options_form()
+{
+?>
+	<h3><?php _e('Limit Blog Registrations Per User') ?></h3>
+	<table>
+	<tbody>
+		<tr valign="top"> 
+				<th scope="row">Number of blogs allowed per User</th> 
+				<td>
+					<input type="text" name="num_allowed_blogs" value="<?php echo tiw_num_allowed_blogs()?>" />
+					<p>If the Value is Zero,It indicates any number of blog is allowed</p>
+				</td>
+		</tr>
+	</tbody>
+	</table>
+<?php
+}
+
+/**************Save the Number of blogs per user when the form is updated **************/
+function tiw_save_num_allowed_blogs()
+{
+$allowed_number_of_blogs=intval($_POST["num_allowed_blogs"]);//how many blogs the user has set
+//save to the database
+update_site_option("tiw_allowed_blogs_per_user",$allowed_number_of_blogs);//now update
+
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Limit Blogs Per User ===
+Contributors: write2brajesh
+Tags: buddypress,wpmu, options,limit blogs,user,limit
+Requires at least: 2.5+
+Tested up to: 2.7.1
+Stable tag: 1.0
+
+This plugin is for wpmu(wordpress Mu) and/or wpmu+buddypress based social network.It limits the number of blogs a user can create.
+== Description ==
+
+This is a plugin for wpmu/wpmu+byddypress powered websites,where site administrators can limit the number of blogs a user can signup.
+It is pretty simple and adds an option to wpmu SiteAdmin->options menu,where you can limit the number of blogs.No additional frills required.
+
+What It does
+
+It adds an option to the options page when you are logged in as site admin of the wpmu site(or wpmu+buddyppress site),Look at the bottom of options page,and You will see a text box like this asking for number of blogs allowed per user.If you set it to zero(which is the default),It will not restrict the blog registration then.
+
+ == Installation ==
+
+1. Download the plugin Limit Blogs Per Users 
+2. Unzip the plugin and upload the file "limit-bogs-per-user.php"  to your mu-plugins folder
+
+3 Now The plugin will be automatically activated,please login to the wordpress backend as Site Administrator(site owner)
+
+4.Go to SiteAdmin-> Options page  and navigate to the bottom,you will see an option to enter the number of blogs allowed,per 
+user.Enter some limit and click update.
+
+5.You are done!.Now it's time to for action.Enjoy the life.
+
+== Frequently Asked Questions ==
+
+Please leave a comment on my plugin page 
+
+[here]("http://www.thinkinginwordpress.com/2009/03/limit-number-of-blogs-per-user-for-wordpress-mu-and-buddypress-websiteblog-network/") for any questions.
+
+
+
+== Screenshots ==
+Please visit 
+
+[http://www.thinkinginwordpress.com/2009/03/limit-number-of-blogs-per-user-for-wordpress-mu-and-buddypress-websiteblog-network/](http://www.thinkinginwordpress.com/2009/03/limit-number-of-blogs-per-user-for-wordpress-mu-and-buddypress-websiteblog-network/) 
+
+==Other Notes==
+For any support or any questions,please leave a comment at my blog 
+
+[http://www.thinkinginwordpress.com/2009/03/limit-number-of-blogs-per-user-for-wordpress-mu-and-buddypress-websiteblog-network/]("http://www.thinkinginwordpress.com/2009/03/limit-number-of-blogs-per-user-for-wordpress-mu-and-buddypress-websiteblog-network/" )
